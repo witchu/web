14be44d24b77339e37ee91170934464dfd60a7af
Witchu Promjunyakul <witchu@debuz.com>
2023-06-11 19:29:18 +0700

    init

---
+++ .eleventy.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const htmlmin = require("html-minifier");
+const yaml = require("js-yaml");
+
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+
+  if (process.env.ELEVENTY_PRODUCTION) {
+    eleventyConfig.addTransform("htmlmin", htmlminTransform);
+  }
+
+  // Passthrough
+  eleventyConfig.addPassthroughCopy({ "src/static": "." });
+
+  // Watch targets
+  eleventyConfig.addWatchTarget("./src/styles/");
+
+  var pathPrefix = "";
+  if (process.env.GITHUB_REPOSITORY) {
+    pathPrefix = process.env.GITHUB_REPOSITORY.split('/')[1];
+  }
+
+  return {
+    dir: {
+      input: "src"
+    },
+    pathPrefix
+  }
+};
+
+function htmlminTransform(content, outputPath) {
+  if (outputPath.endsWith(".html")) {
+    let minified = htmlmin.minify(content, {
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true
+    });
+    return minified;
+  }
+  return content;
+}
